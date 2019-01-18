aedb12c6d4409182ac040313f94b1e57c90b2925
Martin Frouin <mart-33@hotmail.fr>
2019-01-18 16:06:37 +0100

    test answser

--- index.js
+++ index.js
@@ -27,20 +27,20 @@ app.get("/webhook", function(req, res) {
 // All callbacks for Messenger will be POST-ed here
 app.post("/webhook", function(req, res) {
   // Make sure this is a page subscription
-  if (req.body.object == "page") {
-    // Iterate over each entry
-    // There may be multiple entries if batched
-    req.body.entry.forEach(function(entry) {
-      // Iterate over each messaging event
-      entry.messaging.forEach(function(event) {
-        if (event.postback) {
-          processPostback(event);
-        }
-      });
+  // if (req.body.object == "page") {
+  // Iterate over each entry
+  // There may be multiple entries if batched
+  req.body.entry.forEach(function(entry) {
+    // Iterate over each messaging event
+    entry.messaging.forEach(function(event) {
+      if (event.postback) {
+        sendMessage(event.sender.id, "Coucouc");
+      }
     });
+  });
 
-    res.sendStatus(200);
-  }
+  res.sendStatus(200);
+  // }
 });
 
 function processPostback(event) {
