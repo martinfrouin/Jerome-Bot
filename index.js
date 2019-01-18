1b8276f01a6b88e0ca61c3d94212cc4908141756
Martin Frouin <mart-33@hotmail.fr>
2019-01-18 16:11:59 +0100

    test

--- index.js
+++ index.js
@@ -24,75 +24,35 @@ app.get("/webhook", function(req, res) {
   }
 });
 
-// All callbacks for Messenger will be POST-ed here
-app.post("/webhook", function(req, res) {
-  // Make sure this is a page subscription
-  // if (req.body.object == "page") {
-  // Iterate over each entry
-  // There may be multiple entries if batched
-  req.body.entry.forEach(function(entry) {
-    // Iterate over each messaging event
-    entry.messaging.forEach(function(event) {
-      if (event.postback) {
-        sendMessage(event.sender.id, "Coucouc");
-      }
+// Handles messages events
+function handleMessage(sender_psid, received_message) {}
+
+// Handles messaging_postbacks events
+function handlePostback(sender_psid, received_postback) {}
+
+// Sends response messages via the Send API
+function callSendAPI(sender_psid, response) {}
+
+app.post("/webhook", (req, res) => {
+  // Parse the request body from the POST
+  let body = req.body;
+
+  // Check the webhook event is from a Page subscription
+  if (body.object === "page") {
+    body.entry.forEach(function(entry) {
+      // Gets the body of the webhook event
+      let webhook_event = entry.messaging[0];
+      console.log(webhook_event);
+
+      // Get the sender PSID
+      let sender_psid = webhook_event.sender.id;
+      console.log("Sender PSID: " + sender_psid);
     });
-  });
 
-  res.sendStatus(200);
-  // }
-});
-
-function processPostback(event) {
-  var senderId = event.sender.id;
-  var payload = event.postback.payload;
-
-  if (payload === "Greeting") {
-    // Get user's first name from the User Profile API
-    // and include it in the greeting
-    request(
-      {
-        url: "https://graph.facebook.com/v3.2/" + senderId,
-        qs: {
-          access_token: process.env.PAGE_ACCESS_TOKEN,
-          fields: "first_name"
-        },
-        method: "GET"
-      },
-      function(error, response, body) {
-        var greeting = "";
-        if (error) {
-          console.log("Error getting user's name: " + error);
-        } else {
-          var bodyObj = JSON.parse(body);
-          name = bodyObj.first_name;
-          greeting = "Hi " + name + ". ";
-        }
-        var message =
-          greeting +
-          "My name is SP Movie Bot. I can tell you various details regarding movies. What movie would you like to know about?";
-        sendMessage(senderId, { text: message });
-      }
-    );
+    // Return a '200 OK' response to all events
+    res.status(200).send("EVENT_RECEIVED");
+  } else {
+    // Return a '404 Not Found' if event is not from a page subscription
+    res.sendStatus(404);
   }
-}
-
-// sends message to user
-function sendMessage(recipientId, message) {
-  request(
-    {
-      url: "https://graph.facebook.com/v3.2/me/messages",
-      qs: { access_token: process.env.PAGE_ACCESS_TOKEN },
-      method: "POST",
-      json: {
-        recipient: { id: recipientId },
-        message: message
-      }
-    },
-    function(error, response, body) {
-      if (error) {
-        console.log("Error sending message: " + response.error);
-      }
-    }
-  );
-}
+});
