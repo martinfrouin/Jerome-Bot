46c78b75cb4d587a51c714e7ff0f48763448314c
Martin Frouin <mart-33@hotmail.fr>
2019-01-18 16:32:49 +0100

    test

--- index.js
+++ index.js
@@ -25,45 +25,12 @@ app.get("/webhook", function(req, res) {
 });
 
 function handleMessage(sender_psid, received_message) {
-  let response;
-
-  // Check if the message contains text
-  if (received_message.text) {
-    // Create the payload for a basic text message
-    response = {
-      text: `You sent the message: "${
-        received_message.text
-      }". Now send me an image!`
-    };
-  }
-
-  sendMessage(sender_psid, "test");
   // Sends the response message
-  callSendAPI(sender_psid, response);
-}
-
-// sends message to user
-function sendMessage(recipientId, message) {
-  request(
-    {
-      url: "https://graph.facebook.com/v2.6/me/messages",
-      qs: { access_token: process.env.PAGE_ACCESS_TOKEN },
-      method: "POST",
-      json: {
-        recipient: { id: recipientId },
-        message: message
-      }
-    },
-    function(error, response, body) {
-      if (error) {
-        console.log("Error sending message: " + response.error);
-      }
-    }
-  );
+  callSendAPI(sender_psid, received_message.text);
 }
 
 // Handles messaging_postbacks events
-function handlePostback(sender_psid, received_postback) {}
+// function handlePostback(sender_psid, received_postback) {}
 
 function callSendAPI(sender_psid, response) {
   // Construct the message body
@@ -105,15 +72,16 @@ app.post("/webhook", (req, res) => {
 
       // Get the sender PSID
       let sender_psid = webhook_event.sender.id;
-      console.log("Sender PSID: " + sender_psid);
+      // console.log("Sender PSID: " + sender_psid);
 
       // Check if the event is a message or postback and
       // pass the event to the appropriate handler function
       if (webhook_event.message) {
         handleMessage(sender_psid, webhook_event.message);
-      } else if (webhook_event.postback) {
-        handlePostback(sender_psid, webhook_event.postback);
       }
+      // else if (webhook_event.postback) {
+      //   handlePostback(sender_psid, webhook_event.postback);
+      // }
     });
 
     // Return a '200 OK' response to all events
